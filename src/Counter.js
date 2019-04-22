f757c963f9123381ccfffe2ba97a3eb3bb2c9ba2
Shawn Turley <antonioturley@gmail.com>
2019-04-22 12:36:33 -0600

    done

--- src/Counter.js
+++ src/Counter.js
@@ -1,59 +1,86 @@
 import React, { Component } from 'react';
+import store, {INCREMENT, DECREMENT, UNDO, REDO} from './store';
+
 
 class Counter extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      store: store.getState()
+    };
   }
+
+  componentDidMount(){
+    store.subscribe(() => {
+      this.setState({
+        store: store.getState()
+      })
+    })
+  }
+
+  increment = amount => {
+    store.dispatch({type: INCREMENT, amount})
+  }
+  decrement = amount => {
+    store.dispatch({type: DECREMENT, amount})
+  }
+  undo = () => {
+    store.dispatch({type: UNDO})
+  }
+  redo = () => {
+    store.dispatch({type: REDO})
+  }
+  
   render() {
+    const {currentValue, previousValues, futureValues} = this.state.store
     return (
       <div className="app">
         <section className="counter">
-          <h1 className="counter__current-value">{0}</h1>
+          <h1 className="counter__current-value">{currentValue}</h1>
           <div className="counter__button-wrapper">
             <button
               className="counter__button increment-one"
-              onClick={() => null}
+              onClick={() => this.increment(1)}
             >
               +1
             </button>
             <button
               className="counter__button increment-five"
-              onClick={() => null}
+              onClick={() => this.increment(5)}
             >
               +5
             </button>
             <button
               className="counter__button decrement-one"
-              onClick={() => null}
+              onClick={() => this.decrement(1)}
             >
               -1
             </button>
             <button
               className="counter__button decrement-five"
-              onClick={() => null}
+              onClick={() => this.decrement(5)}
             >
               -5
             </button>
             <br />
             <button
               className="counter__button undo"
-              disabled={true}
-              onClick={() => null}
+              disabled={!previousValues.length}
+              onClick={() => this.undo()}
             >
               Undo
             </button>
             <button
               className="counter__button redo"
-              disabled={true}
-              onClick={() => null}
+              disabled={!futureValues.length}
+              onClick={this.redo}
             >
               Redo
             </button>
           </div>
         </section>
         <section className="state">
-          <pre>{JSON.stringify(this.state, null, 2)}</pre>
+          <pre>{JSON.stringify(this.state.store, null, 2)}</pre>
         </section>
       </div>
     );
---
+++ src/store.js
@@ -0,0 +1,45 @@
+import {createStore} from 'redux';
+
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
+export const UNDO = "UNDO";
+export const REDO = "REDO";
+
+const initialState = {
+	currentValue: 0,
+	futureValues: [],
+	previousValues: [],
+}
+
+function reducer(state = initialState, action) {
+	switch (action.type) {
+		case INCREMENT:
+			return {
+				currentValue: state.currentValue + action.amount,
+				futureValues: [],
+				previousValues: [state.currentValue, ...state.previousValues]
+			}
+		case DECREMENT:
+			return {
+				currentValue: state.currentValue - action.amount,
+				futureValues: [],
+				previousValues: [state.currentValue, ...state.previousValues]
+			}
+		case UNDO:
+			return {
+				currentValue: state.previousValues[0],
+				futureValues: [state.currentValue, ...state.futureValues],
+				previousValues: state.previousValues.slice(1)
+			}
+		case REDO:
+			return {
+				currentValue: state.futureValues[0],
+				futureValues: state.futureValues.slice(1),
+				previousValues: [state.currentValue, ...state.previousValues]
+			}
+		default:
+			return state
+	} 
+}
+
+export default createStore(reducer)
